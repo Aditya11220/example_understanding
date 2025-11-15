b99b5f52b851c025bafe349e28402274ea33ffc7
Aditya Hiremath <adityahiremath05@gmail.com>
2025-11-16 02:55:35 +0530

    added mul result

--- ex.py
+++ ex.py
@@ -2,21 +2,30 @@ class Calculator:
 
     @staticmethod
     def add(a,b):
-        return a +b
+        ans= a+b
+        return ans
     
     @staticmethod
     def sub(a,b):
         return a - b
 
     @staticmethod
+    def mul(a,b):
+        return a * b
+    
+    @staticmethod
     def calculator(a, b):
         if "+":
             return Calculator.add(a,b)
-        elif "-":
+        elif "-" :
             return Calculator.sub(a,b)
+        elif "*":
+            return Calculator.mul(a,b)
            
 def main(a,b):
-    sum_result = Calculator.calculator(a,b)
+    sum_result = Calculator.add(a,b)
+    mul_result = Calculator.mul(a,b)
+
     return sum_result
         
 if __name__ == "__main__":
